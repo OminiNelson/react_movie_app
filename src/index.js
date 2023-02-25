0194cb94cd63107fd0776e26f540a0e17ecdfe7e
Omnix <ominimarketme@gmail.com>
2023-02-25 12:28:40 -0800

    chnged from render method to createRoot method

--- src/index.js
+++ src/index.js
@@ -1,17 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+// import ReactDOM from "react-dom";
+import { createRoot } from 'react-dom/client';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
+import App from "./App";
+const root = document.getElementById('root');
+const contentBody = createRoot(root);
+// ReactDOM.render(<App />, document.getElementById('root'));
+contentBody.render(<App />)
